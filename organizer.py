49f1f33291b55136e440e12914c343baeff3153b
Sparks2001 <50083580+wangqinNick@users.noreply.github.com>
2023-01-08 16:10:47 +0800

    Create organizer.py
    File organizer according to the .xx

---
+++ organizer.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+def organize_files(directory):
+    # Create a dictionary to map file types to subdirectories
+    file_types = {}
+
+    # Iterate over the files in the directory
+    for filename in os.listdir(directory):
+        # Ignore hidden files
+        if filename.startswith('.'):
+            continue
+
+        # Get the file extension
+        file_extension = filename.split('.')[-1]
+
+        # If the file extension is not a key in the dictionary, add it
+        # and create a new subdirectory for it
+        if file_extension not in file_types:
+            file_types[file_extension] = file_extension + '_files'
+            os.makedirs(os.path.join(directory, file_types[file_extension]))
+
+        # Move the file to the appropriate subdirectory
+        shutil.move(os.path.join(directory, filename), 
+                    os.path.join(directory, file_types[file_extension], filename))
+
+# Test the function
+organize_files('/Users/qinwang/downloads/')
+print("Heo")
